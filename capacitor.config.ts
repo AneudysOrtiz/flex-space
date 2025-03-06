a034bb16cb9817745905c0e0525946d221607bbb
Aneudys Ortiz <aneudysortizzarzuela@gmail.com>
2025-03-06 00:11:51 -0400

    Add capacitor and ios project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flex.space',
+  appName: 'Flex Space',
+  webDir: 'dist'
+};
+
+export default config;
